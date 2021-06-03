1a7362a99c0033e7a10616eb37a00b119a26195a
aaronchen <aaron.chen@onedegree.hk>
2021-06-03 18:07:32 +0800

    setup tsc and build with tsc

---
+++ packages/components/src/index.ts
@@ -0,0 +1,5 @@
+function components(foo: string, bar: string) {
+  return foo + bar
+}
+
+export default components;
---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+function core(foo: string, bar: string) {
+    return foo + bar
+  }
+
+  export default core;
